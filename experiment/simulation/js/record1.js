ce078be81ef55fd80d48dfe127f15c6a7f1bc252
thakuratul2 <pratapsinghatul111@gmail.com>
2023-04-30 20:11:45 +0530

    Simulation 2

--- experiment/simulation/js/record1.js
+++ experiment/simulation/js/record1.js
@@ -5,7 +5,7 @@ function datasheet1() {
     document.getElementById("w1").innerHTML = "900";
     document.getElementById("degree").innerHTML = "20";
     document.getElementById("Zl").innerHTML = "2.54";
-    document.getElementById("h").innerHTML = "11.78";
+    
 }
 
 function datasheet2() {
@@ -15,9 +15,8 @@ function datasheet2() {
     document.getElementById("w1").innerHTML = "900";
     document.getElementById("degree").innerHTML = "10";
     document.getElementById("Zl").innerHTML = "5.08";
-    document.getElementById("h").innerHTML = "2.32";
+    
 }
-
 function datasheet3() {
     document.getElementById("sno").innerHTML = "1";
     document.getElementById("y2").innerHTML = "21";
@@ -25,7 +24,7 @@ function datasheet3() {
     document.getElementById("w1").innerHTML = "900";
     document.getElementById("degree").innerHTML = "20";
     document.getElementById("Zl").innerHTML = "7.62";
-    document.getElementById("h").innerHTML = "3.31";
+   
 }
 
 function datasheet4() {
@@ -35,7 +34,7 @@ function datasheet4() {
     document.getElementById("w1").innerHTML = "900";
     document.getElementById("degree").innerHTML = "10";
     document.getElementById("Zl").innerHTML = "10.16";
-    document.getElementById("h").innerHTML = "4.57";
+    
 }
 
 
